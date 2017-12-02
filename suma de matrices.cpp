52ef6072f8cc366eaa41c584ce3b2dc054f6e9ea
ElenaRoche <33655419+ElenaRoche@users.noreply.github.com>
2017-12-02 01:54:57 +0100

    Add files via upload

---
+++ suma de matrices.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int salir;
+    int matriz_1[2][2];
+    int matriz_2[2][2];
+    int matriz_suma[2][2];
+    
+    
+    
+    
+    std::cout<<"toca cualquier tecla para salir/n";
+    std::cin>>salir;
+    return 0;
+    
+}
